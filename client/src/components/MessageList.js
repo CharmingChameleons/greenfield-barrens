177171ce6083da939b7689e054adc076e0a51cdf
Shi-Hao Hong <shihaohong94@gmail.com>
2017-07-07 19:54:23 -0700

    added auto-scroll

--- client/src/components/MessageList.js
+++ client/src/components/MessageList.js
@@ -1,21 +1,47 @@
 import React from 'react';
-
+import ReactDOM from 'react-dom';
 import MessageListItem from './MessageListItem';
 
-const MessageList = ({ messages, user }) => {
-  const messagesInChannel = messages.filter(message => message.channel === user.channel);
-  const messageItems = messagesInChannel.length
-    ? messagesInChannel.map(message =>
-        <MessageListItem message={message} user={user} />
-      )
-    : <h4 className="message-prompt"> Get the conversation going! </h4>;
-  return (
-    <div className="message-body">
-      {user.region === '... nowhere ...'
-        ? <h4 className="message-prompt">Please wait for login...</h4>
-        : messageItems}
-    </div>
-  );
-};
+class MessageList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  scrollToBottom() {
+    const node = ReactDOM.findDOMNode(this.messagesEnd);
+    node.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+
+  renderMessages() {
+    const messagesInChannel = this.props.messages.filter(message => message.channel === this.props.user.channel);
+    return messagesInChannel.length
+      ? messagesInChannel.map(message =>
+          <MessageListItem message={message} user={this.props.user} />
+        )
+      : <h4 className="message-prompt"> Get the conversation going! </h4>;
+  }
+
+  render() {
+
+    return (
+      <div className="message-body">
+        {this.props.user.region === '... nowhere ...'
+          ? <h4 className="message-prompt">Please wait for login...</h4>
+          : this.renderMessages() }
+        <div style={{ float:"left", clear: "both" }}
+             ref={(el) => { this.messagesEnd = el; }} />
+      </div>
+
+    )
+  }
+}
 
 export default MessageList;
