86fb512e817c6d27ec5c5dc9f899f1b79063c4a6
Henri Pietila <henrismail@gmail.com>
2017-07-08 14:02:43 -0700

    file upload?

--- client/src/components/MessageInput.js
+++ client/src/components/MessageInput.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+//const SocketIOFileUpload = require('socketio-file-upload');
+
 
 class MessageInput extends Component {
   constructor(props) {
@@ -12,13 +14,15 @@ class MessageInput extends Component {
 
   handleChange(e) {
     this.setState({
-      input: e.target.value
+      input: e.target.value,
     });
   }
 
   handleSubmit(e) {
     e.preventDefault();
+    console.log(document.getElementById("siofu_input").value);
     const inp = this.state.input.trim();
+<<<<<<< 68755fda39eed06a1c7eccfc845c94fac5dbfe0d
     var tempMessage = {
       ...this.props.user,
       text: this.state.input,
@@ -26,11 +30,24 @@ class MessageInput extends Component {
     }
     if (inp && this.props.user.username !== 'Login') {
       this.props.socket.emit('send', tempMessage);
+=======
+    console.log(this.state.image);
+    if ( inp && this.props.user.username !== 'Login') {
+      this.props.socket.emit('send', {
+        ...this.props.user,
+        text: this.state.input,
+        timestamp: new Date().toLocaleTimeString('en-us')
+      });
+>>>>>>> file upload?
       this.setState({
         input: ''
       });
       //this.props.updateMessages(tempMessage)
     }
+  }
+  componentDidMount() {
+    //var uploader = new SocketIOFileUpload(this.props.socket);
+    //uploader.listenOnInput(document.getElementById("siofu_input"));
   }
 
   render() {
@@ -40,6 +57,7 @@ class MessageInput extends Component {
           <div id="message-photo" className="left item">
             <button id="photo-button" className="ui button">
               <i className="photo icon"></i>
+              <input type="file" id="siofu_input" />
             </button>
           </div>
           <div id="message-input" className="left item">
