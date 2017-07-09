b66c2656b29b42703e567040df74ecfd673a4014
priyankacodes <pshah83@gmail.com>
2017-07-08 20:29:08 -0700

    working socket

--- client/src/components/RegionListItem.js
+++ client/src/components/RegionListItem.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const RegionListItem = ({ user, setPage, region, changeRegion, changeChannel } ) => {
+const RegionListItem = ({ user, setPage, region, changeRegion, changeChannel, socket } ) => {
+  function changeSubscription(userRegion, newRegion) {
+    socket.emit('unsubscribe', userRegion);
+    socket.emit('subscribe', newRegion);
+  }
+
   return (
     <div
       className='ui segment' 
       onClick= { 
         function() {
+          changeSubscription(user.region, region.name)
           changeRegion(region.name)
           setPage('landing');
           changeChannel('General')
